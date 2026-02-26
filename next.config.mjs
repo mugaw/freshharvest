a60ec5f5c05f6a41c861095647ba6d10c84a2675
mugaw <muhammedgawad@gmail.com>
2026-02-26 10:49:25 +0200

    Configure basePath and assetPrefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   output: "export",
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
@@ -9,6 +21,6 @@ const nextConfig = {
   },
 };
 
-console.log("Loading next.config.mjs with output: export");
+console.log(`Loading next.config.mjs with output: export, basePath: ${basePath}`);
 
 export default nextConfig;
